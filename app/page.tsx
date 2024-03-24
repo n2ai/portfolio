8d00b717d9aea0ce8570ae3598dab448a839c09f
n2ai <longhai2511@gmail.com>
2024-03-24 18:39:59 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import {Component} from "@/components/component/component";
-import Component2 from "@/components/component/component2";
+
 export default function Home() {
   return (
     <Component/>
